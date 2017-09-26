d4922fac5466dcb9f239e0554122e33207798958
lab <lab@174.194.028.145.hva.nl>
2017-09-26 14:25:41 +0200

    sub-05

--- MB_PRF_convert.py
+++ MB_PRF_convert.py
@@ -7,13 +7,13 @@ import copy, glob, shutil
 #base_dir = '/home/raw_data/2016/visual/whole_brain_MB_pRF/data/Original_data/Original_Raw_Folder/PRF_7T/data/'
 #base_dir = '/home/raw_data/2017/visual/Attention/'
 # base_dir = '/home/raw_data/2017/visual/OriColorMapper/'
-base_dir = '/home/raw_data/2017/reward/pearl_7T/scanner_raw/2017_06_30/sub-04/'
+base_dir = '/home/raw_data/2017/reward/pearl_7T/scanner_raw/2017_07_05/sub-05/'
 #base_dir = '/home/raw_data/2017/visual/nPRF/raw/'
 ref_name = 'ref32rfsp47x47x47'
 
 # raw_dir_name = 'DE_20062016' # 'NA_21062016', 'MB_21062016', 'TK_21062016'
 #for raw_dir_name in ['AU_05072016','AV_06072016','BM_06072016','EO_06072016','IV_05072016','JL_06072016']:  #  'MB_21062016', 'TK_21062016']:
-for index, raw_dir_name in enumerate(['su_9403','su_9404']):
+for index, raw_dir_name in enumerate(['su_9600','su_9601']):
     pp = raw_dir_name[:2] + str(index+1)
 
     out_dir = os.path.join(base_dir, pp, 'mri')
